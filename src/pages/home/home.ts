7a9aef033d2f4e727d133bad37ff3f32a72c1a79
christiantola <christian.tola@hotmail.com>
2018-04-17 12:07:48 -0400

    add service and home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SearchService } from '../../providers/search-service/search-service';
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  students: string[] = [];
 
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public searchService: SearchService
+  ) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+    this.searchService.getStudents().subscribe(
+      (data) => {
+        this.students = data['results'];
+      }
+    )
   }
 
 }
---
+++ src/providers/search-service/search-service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the SearchServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class SearchService {
+
+  readonly URL: string = 'http://localhost:8080/rest/students';
+
+  constructor(public http: HttpClient) {
+    console.log('Hello SearchServiceProvider Provider');
+    console.log(this.URL);
+  }
+
+  getStudents() {
+    return this.http.get(this.URL);
+  }
+
+  searchStudent(query: string) {
+
+  }
+}
